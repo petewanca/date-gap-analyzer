27ef3b340ad0d3cee8d81a6f2bded85a794ddf51
Pete Wanca <pwanca@xsolis.corp>
2020-02-11 11:19:02 -0600

    adds csv retrieval

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const parse = require('csv-parse')
-const assert = require('assert')
-const output = []
-// Create the parser
-const parser = parse({
-  delimiter: ':'
-})
+// set dependencies
+const CSVToJSON = require('csvtojson');
+const JSONToCSV = require('json2csv');
+const FileSystem = require('fs');
+
+// set variable to hold response from csv grab.
+let data;
+
+// retrieve data from loaded csv and set to local variable for manipulation
+CSVToJSON().fromFile('assets/redacted_success.csv').then(source => {
+    data = source;
+});
