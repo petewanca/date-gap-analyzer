11ccbf6b7ffa36017a8f136cf5212d62bbcd6893
Pete Wanca <pwanca@xsolis.corp>
2020-02-11 16:49:51 -0600

    refactoring loop

--- app.js
+++ app.js
@@ -3,32 +3,46 @@ const CSVToJSON = require('csvtojson');
 const JSONToCSV = require('json2csv');
 const moment = require('moment');
 const FileSystem = require('fs');
-
-// set variable to hold response from csv grab.
 let data = [];
+let counter = 1;
+
+
 // retrieve data from loaded csv and set to local variable for manipulation
 CSVToJSON().fromFile('assets/redacted_success.csv').then(source => {
     data = source;
-}).then(() => {
-    
+
     for (let i = 0; i < data.length; i++) {
-        let maxMin = data[i].MAX_MINUTES;
-        let dateOne = moment(data[i].DateLoaded)
-        let dateTwo = moment(data[i+1].DateLoaded)
-        let diff = dateOne.diff(dateTwo, 'minutes');
+        let maxMin = 60;
+        let dateOne = moment(data[i].DateLoaded);
+        // console.log(dateOne);
+        let dateTwo = moment(data[i++].DateLoaded);
+        // console.log(dateTwo);
+        let difference; //= dateOne.diff(dateTwo, 'minutes'); 
+        // console.log(diff);
 
         if ((dateOne) && (dateTwo)) {
-            if (diff > maxMin) {
-                console.log(`uh oh! diff: ${diff}`);
-            }
+            difference = dateOne.diff(dateTwo, 'minutes'); 
         }
+        console.log('hello')
+        if (difference > maxMin) {
+            // setInterval(() => {
+                console.log(`max minutes exceeded: ${difference}`); 
+            i++;
+        } else if ((!dateOne) || (!dateTwo)) {
+            console.log('all done')
+        } else i++;
     }
 
+        
 
-    // let dateOne = moment(data[0].DateLoaded)
-    // let dateTwo = moment(data[1].DateLoaded)
-    // let diff = dateOne.diff(dateTwo, 'minutes');
-    // console.log(diff);
-    
+
 });
 
+
+
+// 1 minute = 60000 miliseconds
+
+// 75000 in 2 mins
+// 37500 in 1 min
+// 625 per second
+// 0.625 per milisecond
