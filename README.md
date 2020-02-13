b1144dbd0ccce8f0b7b3424eeb8e436e16e26214
Pete Wanca <pwanca@xsolis.corp>
2020-02-13 12:03:50 -0600

    adds report time logging and object creation

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # dateGapAnalyzer
 
 ## tech
-- Javascript for logic (Node.js to be specific)
+- Javascript for logic
 - NPM packages (csvtojson & json2csv)
 
 ## about
-Local app used to analyze date/time gaps in CSV files. Use-case is specific a certain job function, but logic can be modified to fit any CSV or business need.
+Local app used to analyze date/time gaps in CSV files for identifying trends. Use-case is specific a certain job function, but logic can be modified to fit any CSV or business need.
 
 ## license 
 MIT
--- app.js
+++ app.js
@@ -4,25 +4,27 @@ const JSONToCSV = require('json2csv').parse;
 const moment = require('moment');
 const fs = require('fs');
 const incidents = []
+const data = []
 
 // retrieve data from loaded csv 
-CSVToJSON().fromFile('assets/redacted_success.csv').then(source => {
-    
+CSVToJSON().fromFile('assets/redacted.csv').then(source => {
+    // log start of report
+    console.log('report started at: ' + new Date());
+    console.log(`preparing ${source.length} records`)
     // use for loop to go through each record
     for (let i = 0; i < source.length; i++) {
-        
         // if next item in array is undefined, report is complete and report generated via csv
         if ((source[i] === undefined) || (source[i+1] === undefined)) {
-            console.log(incidents)
-            console.log('report complete')
             // json to csv conversion
-            const csv = JSONToCSV(incidents, { fields: ['incident'] });
+            const csv = JSONToCSV(incidents, { fields: ['entityId','entityName', 'fileName', 'maxMin', 'timeDiff', 'dateReceived', 'previousFile', 'summary'] });
             // write file to asset folder
             fs.writeFileSync('./assets/report.csv', csv)
+            // log end of report
+            console.log('report ended at: ' + new Date());
+            console.log(`${incidents.length} incidents found.`)
 
         // otherwise, start report
         } else {
-
             // create d/t values out of strings
             let dateOne = moment(source[i].DateLoaded);
             let dateTwo = moment(source[i+1].DateLoaded);
@@ -33,8 +35,21 @@ CSVToJSON().fromFile('assets/redacted_success.csv').then(source => {
 
             // if the difference in time is greater than the minutes allowed before report fires, write to file
             if (timeDiff > maxMin) {
+
                 let record = `Max time was exceeded by ${timeDiff} minutes on ${dateOne}`;
-                incidents.push({'incident': record});
+
+                incidents.push({
+                    'entityId': source[i].ClientId,
+                    'entityName': source[i].ENTITY_NAME,
+                    'fileName': source[i].Name,
+                    'maxMin': maxMin,
+                    'timeDiff': timeDiff,
+                    'dateReceived': dateOne,
+                    'previousFile': dateTwo,
+                    // 'dayOfWeek': moment(dateOne).weekday(),
+                    'summary': record
+                });
+
             }
         }
     }
